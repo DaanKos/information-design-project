7eeb5824922e9cdf54845ee76bb595ac0611957b
Daan <43338443+DaanKos@users.noreply.github.com>
2020-01-20 11:51:00 +0100

    New content creature structure to even/uneven planets to display differently

--- public/scripts/bundle.js
+++ public/scripts/bundle.js
@@ -67,100 +67,190 @@
         d3.select('#parent').selectAll('div').remove();
         let maindivs = d3.select('#parent').selectAll('div').data(givenData).enter().append('div').append('div').attr('class', 'planeetDiv');
 
-        let textdivs = maindivs.append('div').attr('class', 'textDiv');
-            
-        textdivs.append('p')
-            .text(d => d.bedrijfsnaam);
-
-        textdivs.append('p')
-            .text(d => "Winstpercentage: "+(d.perc_winst)+"%");
-
-        maindivs.append('div')
-            .attr('class', 'svgDiv')
-            .append('svg')
-            .attr("width", function(d) {
-                if (d.perc_winst < 1 || d.perc_winst == 'NA') {
-                    return 30;
-                } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) > 180) {
-                    return 180;
-                } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) < 30) {
-                    return 30;
-                } else {
-                    return ((Math.sqrt((d.perc_winst)/(Math.PI)))*45)
-                }
-            })
-            .attr("height", function(d) {
-                if (d.perc_winst < 1 || d.perc_winst == 'NA') {
-                    return 30;
-                } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) > 180) {
-                    return 180;
-                } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) < 30) {
-                    return 30;
-                } else {
-                    return ((Math.sqrt((d.perc_winst)/(Math.PI)))*45)
-                }
-            })
-            .append('circle')
-            .attr("cx", function(d) {
-                if (d.perc_winst < 1 || d.perc_winst == 'NA') {
-                    return 15;
-                } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) > 90) {
-                    return 90;
-                } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) < 15) {
-                    return 15;
-                } else {
-                    return ((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5)
-                }
-            })
-            .attr("cy", function(d) {
-                if (d.perc_winst < 1 || d.perc_winst == 'NA') {
-                    return 15;
-                } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) > 90) {
-                    return 90;
-                } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) < 15) {
-                    return 15;
-                } else {
-                    return ((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5)
-                }
-            })
-            .attr("r", function(d) {
-                if (d.perc_winst < 1 || d.perc_winst == 'NA') {
-                    return 15;
-                } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) > 90) {
-                    return 90;
-                } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) < 15) {
-                    return 15;
-                } else {
-                    return ((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5)
-                }
-            })
-            .style("fill", function(d) {
-                if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'NA' && d.thuiszorg == 'NA') {
-                    return "url(#Rood)";
-                } else if(d.geestelijkegezondheidszorg == 'NA' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'NA') {
-                    return "url(#Geel)";
-                } else if(d.geestelijkegezondheidszorg == 'NA' && d.gehandicaptenzorg == 'NA' && d.thuiszorg == 'ja') {
-                    return "url(#Groen)";
-                } else if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'NA') {
-                    return "url(#GeelRood)";
-                } else if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'NA' && d.thuiszorg == 'ja') {
-                    return "url(#GroenRood)";
-                } else if(d.geestelijkegezondheidszorg == 'NA' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'ja') {
-                    return "url(#GeelGroen)";
-                } else if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'ja') {
-                    return "url(#RoodGeelGroen)";
-                }
+        maindivs.each(function(_, i) {
+            if (i % 2) {
+                appendText(d3.select(this));
+                appendSvg(d3.select(this));
+            } else {
+                appendSvg(d3.select(this));
+                appendText(d3.select(this));
+            }
         });
-    }
 
+        function appendSvg(sel) {
+            sel.append('div')
+                .attr('class', 'svgDiv')
+                .append('svg')
+                .attr("width", function(d) {
+                    if (d.perc_winst < 1 || d.perc_winst == 'NA') {
+                        return 30;
+                    } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) > 180) {
+                        return 180;
+                    } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) < 30) {
+                        return 30;
+                    } else {
+                        return ((Math.sqrt((d.perc_winst)/(Math.PI)))*45)
+                    }
+                })
+                .attr("height", function(d) {
+                    if (d.perc_winst < 1 || d.perc_winst == 'NA') {
+                        return 30;
+                    } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) > 180) {
+                        return 180;
+                    } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) < 30) {
+                        return 30;
+                    } else {
+                        return ((Math.sqrt((d.perc_winst)/(Math.PI)))*45)
+                    }
+                })
+                .append('circle')
+                .attr("cx", function(d) {
+                    if (d.perc_winst < 1 || d.perc_winst == 'NA') {
+                        return 15;
+                    } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) > 90) {
+                        return 90;
+                    } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) < 15) {
+                        return 15;
+                    } else {
+                        return ((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5)
+                    }
+                })
+                .attr("cy", function(d) {
+                    if (d.perc_winst < 1 || d.perc_winst == 'NA') {
+                        return 15;
+                    } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) > 90) {
+                        return 90;
+                    } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) < 15) {
+                        return 15;
+                    } else {
+                        return ((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5)
+                    }
+                })
+                .attr("r", function(d) {
+                    if (d.perc_winst < 1 || d.perc_winst == 'NA') {
+                        return 15;
+                    } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) > 90) {
+                        return 90;
+                    } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) < 15) {
+                        return 15;
+                    } else {
+                        return ((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5)
+                    }
+                })
+                .style("fill", function(d) {
+                    if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'NA' && d.thuiszorg == 'NA') {
+                        return "url(#Rood)";
+                    } else if(d.geestelijkegezondheidszorg == 'NA' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'NA') {
+                        return "url(#Geel)";
+                    } else if(d.geestelijkegezondheidszorg == 'NA' && d.gehandicaptenzorg == 'NA' && d.thuiszorg == 'ja') {
+                        return "url(#Groen)";
+                    } else if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'NA') {
+                        return "url(#GeelRood)";
+                    } else if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'NA' && d.thuiszorg == 'ja') {
+                        return "url(#GroenRood)";
+                    } else if(d.geestelijkegezondheidszorg == 'NA' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'ja') {
+                        return "url(#GeelGroen)";
+                    } else if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'ja') {
+                        return "url(#RoodGeelGroen)";
+                    }
+                });
+        }
+        function appendText(sel) {
+            let textDiv = sel.append('div').attr('class', 'textDiv');
+            
+            textDiv.append('p')
+                    .text(d => d.bedrijfsnaam);
+
+            textDiv.append('p')
+                    .text(d => "Winstpercentage: "+(d.perc_winst)+"%");
+        }
+        // let textdivs = maindivs.append('div').attr('class', 'textDiv');
+            
+        // maindivs.append('div')
+        //     .attr('class', 'svgDiv')
+        //     .append('svg')
+        //     .attr("width", function(d) {
+        //         if (d.perc_winst < 1 || d.perc_winst == 'NA') {
+        //             return 30;
+        //         } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) > 180) {
+        //             return 180;
+        //         } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) < 30) {
+        //             return 30;
+        //         } else {
+        //             return ((Math.sqrt((d.perc_winst)/(Math.PI)))*45)
+        //         }
+        //     })
+        //     .attr("height", function(d) {
+        //         if (d.perc_winst < 1 || d.perc_winst == 'NA') {
+        //             return 30;
+        //         } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) > 180) {
+        //             return 180;
+        //         } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*45) < 30) {
+        //             return 30;
+        //         } else {
+        //             return ((Math.sqrt((d.perc_winst)/(Math.PI)))*45)
+        //         }
+        //     })
+        //     .append('circle')
+        //     .attr("cx", function(d) {
+        //         if (d.perc_winst < 1 || d.perc_winst == 'NA') {
+        //             return 15;
+        //         } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) > 90) {
+        //             return 90;
+        //         } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) < 15) {
+        //             return 15;
+        //         } else {
+        //             return ((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5)
+        //         }
+        //     })
+        //     .attr("cy", function(d) {
+        //         if (d.perc_winst < 1 || d.perc_winst == 'NA') {
+        //             return 15;
+        //         } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) > 90) {
+        //             return 90;
+        //         } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) < 15) {
+        //             return 15;
+        //         } else {
+        //             return ((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5)
+        //         }
+        //     })
+        //     .attr("r", function(d) {
+        //         if (d.perc_winst < 1 || d.perc_winst == 'NA') {
+        //             return 15;
+        //         } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) > 90) {
+        //             return 90;
+        //         } else if (((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5) < 15) {
+        //             return 15;
+        //         } else {
+        //             return ((Math.sqrt((d.perc_winst)/(Math.PI)))*22.5)
+        //         }
+        //     })
+        //     .style("fill", function(d) {
+        //         if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'NA' && d.thuiszorg == 'NA') {
+        //             return "url(#Rood)";
+        //         } else if(d.geestelijkegezondheidszorg == 'NA' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'NA') {
+        //             return "url(#Geel)";
+        //         } else if(d.geestelijkegezondheidszorg == 'NA' && d.gehandicaptenzorg == 'NA' && d.thuiszorg == 'ja') {
+        //             return "url(#Groen)";
+        //         } else if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'NA') {
+        //             return "url(#GeelRood)";
+        //         } else if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'NA' && d.thuiszorg == 'ja') {
+        //             return "url(#GroenRood)";
+        //         } else if(d.geestelijkegezondheidszorg == 'NA' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'ja') {
+        //             return "url(#GeelGroen)";
+        //         } else if(d.geestelijkegezondheidszorg == 'ja' && d.gehandicaptenzorg == 'ja' && d.thuiszorg == 'ja') {
+        //             return "url(#RoodGeelGroen)";
+        //         }
+        // });
+    }
     function passAllFunctions(){
-        var givenValue = document.getElementById("plaats").value;
+        var givenValue = document.getElementById("userGivenPlace").value;
         data.then(result => {
             createViz(parseData(result, givenValue));
         });
     }
     const data = parseCsv(2018);
 
-    document.getElementById("test123").onclick = function() {passAllFunctions();};
+    document.getElementById("userSearchButton").onclick = function() {passAllFunctions();};
 
 }(d3));
